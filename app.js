6d8e31a8ccaf7b1ee3621a5c35419284180cdf53
john <johnhowardm@hotmail.com>
2024-03-14 14:19:37 +0000

    Update

--- app.js
+++ app.js
@@ -1,17 +1,65 @@
+require('dotenv').config();
+
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 const app = express('public');
 
 //register view engine
 app.set('view engine', 'ejs');
 
-app.listen(3000, () => {
-  console.log('Express server listening on port 3000');
-});
+const DB_URI = process.env.MONGODB_URL;
+
+mongoose
+  .connect(DB_URI)
+  .then((result) => {
+    console.log('connected to mongodb');
+    app.listen(3000, () => {
+      console.log('Express server listening on port 3000');
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+// mongoose and mongodb sandbox routes
+app.get('/add-blog', (req, res) => {
+  const blog = new Blog({
+    title: 'adding a new blog',
+    snippet: 'catchy additional blog',
+    body: 'adding another blog - this should be different',
+  });
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => console.log(err));
+});
+
+app.get('/all-blogs', (req, res) => {
+  const allBlogs = Blog.find();
+  allBlogs
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => console.log(err));
+});
+
+app.get('/single-blog', (req, res) => {
+  const singleBlog = Blog.findById('65f1fa8ead2c56fd6471df4e');
+  singleBlog
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => console.log(err));
+});
 
 // middleware
 app.use(morgan('dev'));
@@ -31,21 +79,44 @@ app.use(morgan('dev'));
 app.get('/', (req, res) => {
   //   res.send('<p>Hello from Express World</p>');
   //   res.sendFile('./views/index.html', { root: __dirname });
-  const blogs = [
-    {
-      title: 'Yoshi finds eggs',
-      snippet: 'Lorem ipsum dolor sit amet consectetur',
-    },
-    {
-      title: 'Mario finds stars',
-      snippet: 'Lorem ipsum dolor sit amet consectetur',
-    },
-    {
-      title: 'How to defeat bowser',
-      snippet: 'Lorem ipsum dolor sit amet consectetur',
-    },
-  ];
-  res.render('index', { title: 'Home', blogs });
+  res.redirect('/blogs');
+});
+
+// blog routes
+app.get('/blogs', (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render('index', { title: 'All blogs', blogs: result });
+    })
+    .catch((err) => console.log(err));
+});
+
+app.post('/blogs', (req, res) => {
+  const blog = new Blog(req.body);
+  blog
+    .save()
+    .then((result) => {
+      res.redirect('/blogs');
+    })
+    .catch((err) => console.log(err));
+});
+
+app.get('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then((blog) => {
+      res.render('details', { title: 'Blog details', blog: blog });
+    })
+    .catch((err) => console.log(err));
+});
+app.delete('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({ redirect: '/blogs' });
+    })
+    .catch((err) => console.log(err));
 });
 
 app.get('/about', (req, res) => {
